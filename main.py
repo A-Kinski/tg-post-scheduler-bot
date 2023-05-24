f9c821395dc0faca0a06f14f0b85506cf8ee0ae4
A-Kinski <asidorov90@gmail.com>
2023-05-24 14:53:02 +0300

    added sending text message now

--- main.py
+++ main.py
@@ -1,15 +1,53 @@
 import logging
 
-from pyrogram import Client
+from pyrogram import Client, filters
 import msgspec
+from pyrogram.handlers import MessageHandler, CallbackQueryHandler
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 
 logging.basicConfig(level=logging.INFO)
+
+
 class Config(
     msgspec.Struct,
 ):
     api_id: str
     api_hash: str
     bot_token: str
+    channel_id: str
+
+
+text_message_inline_keyboard = InlineKeyboardMarkup(inline_keyboard=[
+    [
+        InlineKeyboardButton(
+            text='Отправить срочное сообщение',
+            callback_data='send_now'
+        )
+    ],
+    [
+        InlineKeyboardButton(
+            text='Отправить отложенное сообщение',
+            callback_data='send_scheduled'
+        )
+    ]
+])
+
+
+# Обработка нажатия на кнопку "Отправить сейчас"
+def send_text_now(client: Client, call: CallbackQuery):
+    call.message.reply_to_message.copy(chat_id='test_for_bot_a_kinski')
+
+
+def message_text(client: Client, message: Message):
+    message.reply('Ты хочешь отправить это сообщение?', quote=True, reply_markup=text_message_inline_keyboard)
+
+
+# Функция для создания фильтров сообщений
+def call_data(data):
+    async def filter_data(self, __, call: CallbackQuery):
+        return self.data == call.data
+
+    return filters.create(filter_data, data=data)
 
 
 def main():
@@ -19,10 +57,9 @@ def main():
                     api_id=config.api_id,
                     api_hash=config.api_hash,
                     bot_token=config.bot_token)
-    client.start()
-    client.send_message(chat_id='AntKinski', text='это сообщение я отправил из python программы')
-    client.stop()
-    logging.info("sending messages")
+    client.add_handler(MessageHandler(message_text, filters=filters.text))
+    client.add_handler(CallbackQueryHandler(send_text_now, call_data('send_now')))
+    client.run()
 
 
 if __name__ == '__main__':
